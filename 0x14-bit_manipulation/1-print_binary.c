bec6d6148a5e689d25b971fbf57f02b608aad963
Abdellah31 <azizlive1900@gmail.com>
2023-05-04 08:20:09 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,7 +3,6 @@
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */
-
 void print_binary(unsigned long int n)
 {
 int i, count = 0;
@@ -13,12 +12,12 @@ for (i = 63; i >= 0; i--)
 current = n >> i;
 if (current & 1)
 {
-	_putchar('1');
+_putchar('1');
 count++;
-	}
+}
 else if (count)
 _putchar('0');
-	 }
+}
 if (!count)
-	_putchar('0');
+_putchar('0');
 }
