56d6f7bfa68dd29cb4e85235c4998603750a2054
Abdellah31 <azizlive1900@gmail.com>
2023-05-08 17:50:54 -0700

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,24 +5,24 @@
  * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
  * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
+ * Return: r- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	ssize_t fd;
+	ssize_t fl;
 	ssize_t w;
-	ssize_t t;
+	ssize_t p;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fl = open(filename, O_RDONLY);
+	if (fl == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	p = read(fl, buf, letters);
+	r = write(STDOUT_FILENO, buf, p);
 
 	free(buf);
-	close(fd);
-	return (w);
+	close(fl);
+	return (r);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,24 +10,24 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int oi, wi, k = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (k = 0; text_content[k];)
+			k++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	oi = open(filename, O_WRONLY | O_APPEND);
+	wi = write(oi, text_content, k);
 
-	if (o == -1 || w == -1)
+	if (oi == -1 || wi == -1)
 		return (-1);
 
-	close(o);
+	close(oi);
 
 	return (1);
 }
